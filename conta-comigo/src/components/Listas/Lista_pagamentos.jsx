dc8e14a0268fc8fef85d1ad1bd40184378d0f8ea
Gdmasceno <gabriel.doliveira@sptech.school>
2023-05-27 20:46:57 -0300

    atualizando front da tela inicio

--- conta-comigo/src/components/Listas/Lista_pagamentos.jsx
+++ conta-comigo/src/components/Listas/Lista_pagamentos.jsx
@@ -1,12 +1,18 @@
 import styles from "../../_assets/css/modules/listas modules/lista_pagamentos.module.css"
-import pagamento from "../../_assets/img/pagamento.png"
+import pagamento from "../../_assets/img/icons/celpix.png"
 
 function Lista_pagamentos(){
     return(
         <div className={styles.pagamento}>
-            <div className={styles.titulo}>Pagamento via pix</div>
-            <div className={styles.desc_mesa}>Mesa 2</div>
-            <img src={pagamento} alt="" />
+            <div className={styles.celular}>
+                <img src={pagamento} alt="" />
+            </div>
+            <div className={styles.dados}>
+                <div className={styles.preco}>
+                <div className={styles.money}>R$ 8,00</div>
+                <div className={styles.desc}>Mesa 2 - Gabriel</div>
+                </div>
+            </div>
         </div>
     );
 }
