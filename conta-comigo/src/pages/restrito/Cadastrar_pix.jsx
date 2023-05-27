1c6074e94387d9c587c9656eaef8fd99743416be
cardosoOReis <rafaelcdsreis@gmail.com>
2023-05-27 19:55:07 -0300

    fix envio de arquivo

--- conta-comigo/src/pages/restrito/Cadastrar_pix.jsx
+++ conta-comigo/src/pages/restrito/Cadastrar_pix.jsx
@@ -1,90 +1,94 @@
 import LateralMenu from "../../components/Lateral_menu";
-import styles from "../../_assets/css/modules/core modules/cadastrar_pix.module.css"
-import pasta from "../../_assets/img/icons/pasta.png"
-import voltar from "../../_assets/img/icons/setaVoltar.png"
-import {useNavigate} from "react-router-dom";
+import styles from "../../_assets/css/modules/core modules/cadastrar_pix.module.css";
+import pasta from "../../_assets/img/icons/pasta.png";
+import voltar from "../../_assets/img/icons/setaVoltar.png";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import api from "../../config/api";
 import { useState } from "react";
 
-
-
 function Cadastrar_pix() {
-    const navigate = useNavigate()
-    const [chavepix, setChavepix] = useState()
-    const [clienteid, setClienteid] = useState()
-    const [clienteSecret, setClienteSecret] = useState()
+  const navigate = useNavigate();
+  const [chavepix, setChavepix] = useState();
+  const [clienteid, setClienteid] = useState();
+  const [clienteSecret, setClienteSecret] = useState();
 
-    const changeChave = (event) => {
-        setChavepix(event.target.value);
-    };
+  const changeChave = (event) => {
+    setChavepix(event.target.value);
+  };
 
-    const changeId = (event) => {
-        setClienteid(event.target.value);
-    };
+  const changeId = (event) => {
+    setClienteid(event.target.value);
+  };
 
-    const changeSecret = (event) => {
-        setClienteSecret(event.target.value);
-    };
+  const changeSecret = (event) => {
+    setClienteSecret(event.target.value);
+  };
 
-    function cadastrarChave() {
-        console.log(chavepix, clienteid, clienteSecret)
-        api.put("/detalhes-pagamento/criar/" + sessionStorage.userId, {
-            chavePix: chavepix,
-            clientId: clienteid,
-            clientSecret: clienteSecret
-        })
-            .then((response) => {
-                console.log("RESPONSE: ", response)
-                Swal.fire(
-                    'Chave pix cadastrada!',
-                    '',
-                    'success'
-                ).then((value) => {
-                    navigate("/perfil")
-                })
-                // getPedidos();
+  function cadastrarChave() {
+    console.log(chavepix, clienteid, clienteSecret);
+    api
+      .put("/detalhes-pagamento/criar/" + sessionStorage.userId, {
+        chavePix: chavepix,
+        clientId: clienteid,
+        clientSecret: clienteSecret,
+      })
+      .then((response) => {
+        console.log("RESPONSE: ", response);
+        Swal.fire("Chave pix cadastrada!", "", "success").then((value) => {
+          navigate("/perfil");
+        });
+        // getPedidos();
+      })
+      .catch((err) => {
+        console.log("TINHA QUE ENTRAR AQUI");
+        console.log(err.response.data.errors[0].defaultMessage);
+      });
+  }
+  if (sessionStorage.length > 0) {
+    // var infoAtv = 'infoAtv';
+    return (
+      <div className="fBody">
+        <LateralMenu />
+        <div className={styles.main}>
+          <div
+            onClick={() => {
+              navigate("/perfil");
+            }}
+            className={"voltar"}
+          >
+            <img src={voltar} alt="" />
+            <p>voltar</p>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.container_top}>
+              <p>Cadastre aqui a sua chave PIX!</p>
+              <div className={styles.passagem}>
+                <div className={styles.line}></div>
+              </div>
+            </div>
 
-            }).catch((err) => {
-                console.log("TINHA QUE ENTRAR AQUI")
-                console.log(err.response.data.errors[0].defaultMessage)
+            <div className={styles.container_main}>
+              <input
+                type="text"
+                className={styles.input_valor}
+                placeholder="Digite a chave PIX:"
+                onChange={changeChave}
+              />
+              <input
+                type="text"
+                className={styles.input_valor}
+                placeholder="Client ID:"
+                onChange={changeId}
+              />
+              <input
+                type="text"
+                className={styles.input_valor}
+                placeholder="Client Secret:"
+                onChange={changeSecret}
+              />
 
-
-
-            })
-    }
-    if (sessionStorage.length > 0) {
-
-        // var infoAtv = 'infoAtv';
-        return (
-
-            <div className="fBody">
-                <LateralMenu />
-                <div className={styles.main}>
-                    <div onClick={() => { navigate("/perfil") }} className={"voltar"}>
-                        <img src={voltar} alt="" />
-                        <p>voltar</p>
-                    </div>
-                    <div className={styles.container}>
-                        <div className={styles.container_top}>
-                            <p>Cadastre aqui a sua chave PIX!</p>
-                            <div className={styles.passagem}>
-                                <div className={styles.line}></div>
-
-
-                            </div>
-
-                        </div>
-
-                        <div className={styles.container_main}>
-
-
-                            <input type="text" className={styles.input_valor} placeholder="Digite a chave PIX:" onChange={changeChave} />
-                            <input type="text" className={styles.input_valor} placeholder="Client ID:" onChange={changeId} />
-                            <input type="text" className={styles.input_valor} placeholder="Client Secret:" onChange={changeSecret} />
-
-
-                            {/* <div className={styles.file}>
+              {/* <div className={styles.file}>
 
                                 <form>
                                     <label for="file"> Certificado:
@@ -95,65 +99,61 @@ function Cadastrar_pix() {
 
                             </div> */}
 
-                            <div className={styles.arquivo} onClick={async () => {
-                                const { value: file } = await Swal.fire({
-                                    title: 'Select image',
-                                    input: 'file',
-                                    inputAttributes: {
-                                        'accept': '*',
-                                        'aria-label': 'Adicione o certificado'
-                                    }
-                                })
+              <div
+                className={styles.arquivo}
+                onClick={async () => {
+                  const { value: file } = await Swal.fire({
+                    title: "Select image",
+                    input: "file",
+                    inputAttributes: {
+                      accept: "*",
+                      "aria-label": "Adicione o certificado",
+                    },
+                  });
 
-                                if (file) {
-                                    let data = new FormData()
-                                    data.append('certificado',file)
-                                    let config = {
-                                        method: 'put',
-                                        maxBodyLength: Infinity,
-                                        url: '/detalhes-pagamento/certificados/criar/'+ sessionStorage.userId,
-                                        headers: { 
-                                          'Authorization': 'Bearer ' + sessionStorage.token
-                                        },
-                                        data : data
-                                      };
-                                    console.log(data)
-
-                                    api.request(config)
-                                    .then((response) => {
-                                            console.log("RESPONSE: ", response)
-                                            Swal.fire(
-                                                'Documento adicionado!',
-                                                '',
-                                                'success'
-                                            )
-                                            // getPedidos();
-                                            
-                                        }).catch((err) => {
-                                            console.log("TINHA QUE ENTRAR AQUI")
-                                            console.log(err)                                                        
-                                        })
-                                }
-                            }}>
-                                <p>Adicionar certificado</p>
-                                <img src={pasta} alt="" />
-                            </div>
-                        </div>
-                        <div className={styles.buttons}>
-                            <button className={styles.button_two} onClick={cadastrarChave}>Cadastrar Chave</button>
-                        </div>
-
-                    </div>
-
-
-                </div>
+                  if (file) {
+                    let data = new FormData();
+                    data.append("certificado", file);
+                    api
+                      .put(
+                        `/detalhes-pagamento/certificados/criar/${sessionStorage.getItem(
+                          "userId"
+                        )}`,
+                        data,
+                        {
+                          headers: {
+                            "Content-Type": "multipart/form-data",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        console.log("RESPONSE: ", response);
+                        Swal.fire("Documento adicionado!", "", "success");
+                        // getPedidos();
+                      })
+                      .catch((err) => {
+                        console.log("TINHA QUE ENTRAR AQUI");
+                        console.log(err);
+                      });
+                  }
+                }}
+              >
+                <p>Adicionar certificado</p>
+                <img src={pasta} alt="" />
+              </div>
             </div>
-        );
-    }
-
-    else {
-        window.location.href = "http://localhost:3000/login";
-    }
+            <div className={styles.buttons}>
+              <button className={styles.button_two} onClick={cadastrarChave}>
+                Cadastrar Chave
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    window.location.href = "http://localhost:3000/login";
+  }
 }
 
 export default Cadastrar_pix;
