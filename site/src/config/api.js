1add940ed09065caa1908b7581327b0d774cf04c
Gdmasceno <gabriel.doliveira@sptech.school>
2023-05-28 22:22:35 -0300

    configurando api com a ec2

--- site/src/config/api.js
+++ site/src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
+    baseURL: "http://ubuntu@ec2-3-23-197-186.us-east-2.compute.amazonaws.com:8080",
     headers: { 
         'Authorization': sessionStorage.token ? `Bearer ${sessionStorage.token}` : ''
       }
