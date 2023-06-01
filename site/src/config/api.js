b8fc67259c64bdc0a1380dfdd22d78bd40793365
Gdmasceno <gabriel.doliveira@sptech.school>
2023-06-01 16:30:08 -0300

    atualização deploy

--- site/src/config/api.js
+++ site/src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:8080",
-    baseURL: "ec2-52-39-227-213.us-west-2.compute.amazonaws.com:8080",
+    baseURL: "http://ec2-52-39-227-213.us-west-2.compute.amazonaws.com:8080",
     // baseURL: "http://ubuntu@ec2-3-23-197-186.us-east-2.compute.amazonaws.com:8080",
     headers: { 
         'Authorization': sessionStorage.token ? `Bearer ${sessionStorage.token}` : ''
